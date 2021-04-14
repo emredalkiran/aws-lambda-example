3605a61f1343ad444a12b12a227076552204b9a1
emredalkiran <emre59dalkiran@gmail.com>
2021-04-15 00:38:58 +0300

    Deleted connection url code from mongodb atlas

--- utils/database.ts
+++ utils/database.ts
@@ -17,16 +17,3 @@ export class MongoConnection implements DatabaseConnection {
     return db
   }
 }
-
-/* 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:<password>@cluster0.f05vu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-
-*/
